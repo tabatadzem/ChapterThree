0b309efca54a06c86473c8d50a01b1094d6396c9
tabatadzem <tabatadzem@hotmail.com>
2017-04-25 08:42:43 +0200

    exercise 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,32 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int number1 = (int)(System.currentTimeMillis() % 10);
-        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
-        Scanner input = new Scanner(System.in);
-        System.out.print("What is " + number1 + " + " + number2 + "? ");
-        int answer = input.nextInt();
-        System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+//        int number1 = (int)(System.currentTimeMillis() % 10);
+//        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("What is " + number1 + " + " + number2 + "? ");
+//        int answer = input.nextInt();
+//        System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
 
+//    Scanner input = new Scanner(System.in);
+//    System.out.println("please enter the radius");
+//    int radius = input.nextInt();
+//    double area;
+//    final double PI = 3.14;
+//        if (radius >= 0) {
+//            area = radius * radius * PI;
+//            System.out.println("The area for the circle of radius " + radius + " is " + area);
+//        }
+
+    Scanner input = new Scanner(System.in);
+    System.out.println(" please enter any number");
+    int number = input.nextInt();
+    if (number%5 == 0){
+        System.out.println("HiFive");
+    }
+    if (number%2 == 0){
+        System.out.println("HiEven");
+    }
 
 
 
