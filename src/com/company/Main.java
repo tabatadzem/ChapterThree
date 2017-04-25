f3b6d0db06be156937eb89a8dfeed68505bf4498
tabatadzem <tabatadzem@hotmail.com>
2017-04-25 08:04:06 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+//        int number1 = (int)(System.currentTimeMillis() % 10);
+//        int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("What is " + number1 + " + " + number2 + "? ");
+//        int answer = input.nextInt();
+//        System.out.println(number1 + " + " + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+
+        int x= 1;
+        System.out.print(x<0);
+
+
+    }
+}
